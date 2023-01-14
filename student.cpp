9e3318a22680867c792edb41e3058ed38f8b8436
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-01-13 19:39:23 -0800

    Formatting changes

--- student.cpp
+++ student.cpp
@@ -1,5 +1,17 @@
 
  #include <iostream>
  #include <cstring>
- #include "student.h"
+ #include "Student.h"
  using namespace std;
+
+ Student::Student(char* name1) {
+   strncpy(name, name1, 20);  
+ }
+
+ char* Student::getName() {
+   return name; 
+ }
+
+ Student::~Student() {
+  delete name;
+ }
