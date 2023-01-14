8777a1bebaf8b52099af9e18f81c71c0f5af5434
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-01-13 16:29:37 -0800

    Create the getnext and get student pointers and also created the destructor

--- node.h
+++ node.h
@@ -1,13 +1,20 @@
 
- #ifndef NODE_H
- #define NODE_H
-
+ #pragma once
  #include <iostream>
  #include <cstring>
- #include "student.h"
+ #include "Student.h"
  using namespace std;
 
  class Node{
+  public:
+   Node(Student*);
+   Node* getNext();
+   Student* getStudent();
+   void setNext(Node*);
+   ~Node();
 
- }
+   Student* nextStu;
+   Node* nextPointer;
+
+ };
  
