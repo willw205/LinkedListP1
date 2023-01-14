7ea9f9ce3c52c34671b655391a7b721958f36240
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-01-13 19:38:50 -0800

    Create getNext, getStudent and set next functions

--- node.cpp
+++ node.cpp
@@ -1,6 +1,31 @@
 
  #include <iostream>
  #include <cstring>
- #include "student.h"
- #include "node.h"
+ #include "Student.h"
+ #include "Node.h"
  using namespace std;
+
+ Node::Node(Student* student1) {
+   nextStu = student1;
+ }
+
+ Node* Node::getNext() {
+    return next;
+  }
+
+ Student* Node::getStudent() {
+
+   return nextStu;
+   
+ }
+
+ void Node::setNext(Node* next1) {
+   next = next1;
+ }
+
+
+ Node::~Node() {
+   cout << "demolished!" << endl;
+ }
+   
+  
